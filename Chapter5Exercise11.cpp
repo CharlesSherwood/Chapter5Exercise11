d74bb361fc04b1d5e332c6057b271425ff3d3086
Charles Sherwood <kulan876321@yahoo.com>
2025-03-05 11:45:05 -0500

    Add project files.

---
+++ Chapter5Exercise11.cpp
@@ -0,0 +1,71 @@
+/*FileName:Chapter5Exercise11.cpp
+ProgrammerName:Charles Sherwood
+Date:3/25
+requirements:
+
+
+
+
+*/
+
+
+
+#include<iomanip>
+#include<cmath> 
+#include <iostream>
+using namespace std;
+
+double Orginism()
+{
+	double orginism;
+	cout << "Please enter the starting number of orginisims:";
+	cin >> orginism;
+
+
+
+
+	return orginism;
+}
+double PopInc()
+{
+	double popinc;
+	cout << "Please enter the amount the population increased";
+	cin >> popinc;
+
+
+
+	return popinc;
+}
+
+double NumOfDays()
+{
+	double num_of_days;
+	cout << "Please enter the number of days";
+	cin >> num_of_days;
+
+
+
+
+
+	return num_of_days;
+}
+
+
+int main()
+{
+	double population = Orginism();
+	double popinc = PopInc();
+	double num_of_days = NumOfDays();
+
+
+
+
+	
+	
+	
+	
+	
+	
+	
+}
+
