2a582d356bfe4dadcefdf0871c6d0f21a9f6a2de
Charles Sherwood <kulan876321@yahoo.com>
2025-04-05 12:53:34 -0400

    Finished

--- Chapter5Exercise11.cpp
+++ Chapter5Exercise11.cpp
@@ -2,76 +2,94 @@
 ProgrammerName:Charles Sherwood
 Date:3/25
 requirements:This program will determine the size of a 
-population of orginisms
-
-
-
-
+population of orginisms.
 */
 
 
-
-#include<iomanip>
-#include<cmath> 
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
 
-double Orginism()
-{
-	double orginism;
-	cout << "Please enter the starting number of orginisims:";
-	cin >> orginism;
-	while (orginism < 2)
-	{
-		cout << "Please Enter A Valid Amount:";
-		cin >> orginism;
-	}
-	return orginism;
-}
-double PopInc()
-{
-	double popinc;
-	cout << "Please enter the amount the population increased";
-	cin >> popinc;
-	while (popinc < 0)
-	{
-		cout << "Please Enter A New Amount:";
-		cin >> popinc;	
-	}
+// Function prototypes
+int getStartingPopulation();
+double getDailyIncrease();
+int getNumberOfDays();
+void displayPopulationGrowth(int startPop, double increaseRate, int days);
+bool askToContinue();
 
+// Main function
+int main() {
+    do {
+        int startingPopulation = getStartingPopulation();
+        double dailyIncrease = getDailyIncrease();
+        int numberOfDays = getNumberOfDays();
 
-	return popinc;
+        displayPopulationGrowth(startingPopulation, dailyIncrease, numberOfDays);
+    } while (askToContinue());
+
+    cout << "Thank you for using the population predictor!\n";
+    return 0;
 }
 
-double NumOfDays()
-{
-	double num_of_days;
-	cout << "Please enter the number of days";
-	cin >> num_of_days;
-
-
-
-
-
-	return num_of_days;
+// Function to get validated starting population
+int getStartingPopulation() {
+    int population;
+    do {
+        cout << "Enter the starting number of organisms (minimum 2): ";
+        cin >> population;
+        if (population < 2) {
+            cout << "Error: Population must be at least 2.\n";
+        }
+    } while (population < 2);
+    return population;
 }
 
+// Function to get daily increase percentage
+double getDailyIncrease() {
+    double increase;
+    do {
+        cout << "Enter the average daily population increase (as a percentage): ";
+        cin >> increase;
+        if (increase < 0) {
+            cout << "Error: Daily increase cannot be negative.\n";
+        }
+    } while (increase < 0);
+    return increase;
+}
 
-int main()
-{
-	double population = Orginism();
-	double popinc = PopInc();
-	double num_of_days = NumOfDays();
+// Function to get number of days
+int getNumberOfDays() {
+    int days;
+    do {
+        cout << "Enter the number of days the organisms will multiply (minimum 1): ";
+        cin >> days;
+        if (days < 1) {
+            cout << "Error: Number of days must be at least 1.\n";
+        }
+    } while (days < 1);
+    return days;
+}
 
+// Function to display population growth for each day
+void displayPopulationGrowth(int startPop, double increaseRate, int days) {
+    double population = startPop;
 
+    cout << "\nDay\tPopulation\n";
+    cout << "---------------------\n";
+    cout << fixed << setprecision(2);
 
+    for (int day = 1; day <= days; ++day) {
+        cout << day << "\t" << population << endl;
+        population += population * (increaseRate / 100.0);
+    }
+}
 
-	
-	
-	
-	
-	
-	
-	
+// Function to ask the user if they want to continue
+bool askToContinue() {
+    char choice;
+    cout << "\nWould you like to run the program again? (Y/N): ";
+    cin >> choice;
+    return choice == 'Y' || choice == 'y';
 }
 
